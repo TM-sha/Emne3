cd756a4f8050a2518a653ec2963f645818dd2c0e
Tom86 <tommy_m86@hotmail.com>
2023-11-13 21:40:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Variabel_Oppgaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------ Oppgave 2 Konsoll-applikasjon som lager 3 variabler-------//
+            int a = 5;
+            decimal b = 3.0M;
+            decimal sum = a + b;
+            Console.WriteLine($"Nummeret {a} pluss nummeret {b} blir: {sum}");
+        }
+
+        /*--------Oppgave 1 Lag 8 variabler------*/
+            /*
+            string myName = "Tommy";
+            int myAge = 36;
+            long longNum = 105050501;
+            double myDouble = 19.86;
+            decimal myDecimal = 3.14M;
+            float myFloatNum = 1.05F;
+            bool iLikeFish = false;
+            char myLetter = 'T'; */
+
+            /*--------Oppgave 1 KonsollApp----------*/
+            //Console.WriteLine("Hei, hva heter du?");
+            //var input = Console.ReadLine();
+            //Console.WriteLine($"Velkommen {input}!");
+        }
+    }
