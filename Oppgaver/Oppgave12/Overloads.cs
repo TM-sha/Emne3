0c8b05f4ab12df9577b2d1f8f3cde6addb7dafab
Tom86 <tommy_m86@hotmail.com>
2023-11-23 22:58:02 +0100

    Added new task

---
+++ Oppgaver/Oppgave12/Overloads.cs
@@ -0,0 +1,10 @@
+﻿namespace Variabel_Oppgaver.Oppgaver.Oppgave12
+{
+    internal class Overloads
+    {
+        public static void PrintWelcomeMessage(string name = "Terje")
+        {
+            Console.WriteLine($"Hei og velkommen {name}");
+        }
+    }
+}
