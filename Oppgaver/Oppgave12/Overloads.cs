a5da0961979e700e38ed6f0595182ec5496c0502
Tom86 <tommy_m86@hotmail.com>
2023-11-25 18:02:46 +0100

    Major update: Completed task

--- Oppgaver/Oppgave12/Overloads.cs
+++ Oppgaver/Oppgave12/Overloads.cs
@@ -2,14 +2,40 @@
 {
     internal class Overloads
     {
-        public static void PrintWelcomeMessage()
+        //PrintWelcomeMessage();                        - Hei og velkommen Terje
+        //PrintWelcomeMessage("Tommy");                 - Hei og velkommen Tommy
+        //PrintWelcomeMessage("Gå vekk");               - Hei og velkommen Gå vekk
+        //PrintWelcomeMessage("Kom tilbake", "Stian");  - Kom tilbake, Stian
+
+        public static void PrintWelcomeMessage(string name = "Terje")
         {
-            Console.WriteLine($"Hei og velkommen");
+            Console.WriteLine($"Hei og velkommen {name}");
         }
 
-        public static void PrintWelcomeMessage(string greeting, string name = "Terje")
-        {
-            Console.WriteLine(greeting + " " + name);
-        }
+        //public static void PrintWelcomeMessage(string greeting, string name)
+        //{
+        //    Console.WriteLine(greeting + " " + name);
+        //}
     }
+
+    //internal class MathOverloads
+    //{
+    //    //var result = Add(1, 2)
+    //    public static int Add(int a, int b)
+    //    {
+    //        return a + b;
+    //    }
+
+    //    //var result = Add(1, 2, 3)
+    //    public static int Add(int a, int b, int c)
+    //    {
+    //        return a + b + c;
+    //    }
+
+    //    //var result = Add(1.5, 2.5)
+    //    public static double Add(double a, double b)
+    //    {
+    //        return a + b;
+    //    }
+    //}
 }
